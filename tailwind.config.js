c97a17fce0cb8c6128d5734aad520627eb85025b
Eric Olson <olsoneg@gmail.com>
2024-10-07 11:26:12 -0700

    join page

---
+++ tailwind.config.js
@@ -0,0 +1,11 @@
+module.exports = {
+  content: [
+      './*.html', // Adjust this path to match your project structure
+      './src/**/*.html',
+      './src/**/*.js',
+  ],
+  theme: {
+      extend: {},
+  },
+  plugins: [],
+}
